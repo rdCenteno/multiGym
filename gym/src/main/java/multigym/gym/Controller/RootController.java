c0764914707362c464868bfc7d0979abf7015ea5
rdemera <rdemera@tribalyte.com>
2020-02-18 17:02:37 +0100

    Added basic controller
    -Added some templates
    -Add new static files
    -This commit is to add new files, just to test spring boot

---
+++ gym/src/main/java/multigym/gym/Controller/RootController.java
@@ -0,0 +1,35 @@
+package multigym.gym.Controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class RootController {
+
+    private static Logger log = LogManager.getLogger(RootController.class);
+
+    @GetMapping("/")
+    public String index(Model model, @RequestParam String name) { // viene del formulario
+        model.addAttribute("name", name);
+        log.info("The user has access to /");
+        log.info("The user name is: ", name);
+        return "hello";
+    }
+
+    @GetMapping("/hello")
+    public String hola(Model modelo, @RequestParam String name) {
+        log.info("El usuario ha accedido a la ruta de /hello");
+        modelo.addAttribute("name", name);
+        return "hello";
+    }
+
+    @GetMapping("/otro")
+    public String guessNumber(Model model) {
+        log.info("The user has access to /otro, it returns an empty template");
+        return "pruebaPlantilla";
+    }
+}
