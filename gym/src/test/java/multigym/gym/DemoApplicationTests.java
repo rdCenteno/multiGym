6132d6da79aa1d8e3e8bef71490e440327d21a6e
rdemera <rdemera@tribalyte.com>
2020-02-18 15:14:50 +0100

    First commit, Sring Boot initialization

---
+++ gym/src/test/java/multigym/gym/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package multigym.gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
